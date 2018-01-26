4cd2930d54a8b991d4734a276c7b9e556c0c6a98
NangoliJude <fxjudemedia@gmail.com>
2018-01-26 16:15:18 -0600

    intitial commit

---
+++ src/counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+const styles={ height: "100vh", width: "100vw", padding: "30px"}
+class Counter extends Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+            x: null,
+             y:null
+            }
+            this.mouseMovement = this.mouseMovement.bind(this)
+    }
+    mouseMovement(event){
+        this.setState({x:event.clientX, y:event.clientY})
+    }
+    render(){
+        const {x,y} = this.state;
+        return(
+            <div styles={styles}onMouseMove={this.mouseMovement}>
+                <p>Postion is x: {x} Position y is: {y} </p>
+            </div>
+        )
+    }
+}
+
+export default Counter;
